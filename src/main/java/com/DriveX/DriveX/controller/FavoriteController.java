5a9f48829dde970708e0e9cbfeb03e54d0fd6d73
ignacio <ignaciocastillon2003@gmail.com>
2026-02-26 17:25:45 +0100

    Favorites

--- src/main/java/com/DriveX/DriveX/controller/FavoriteController.java
+++ src/main/java/com/DriveX/DriveX/controller/FavoriteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/favorites")
+@RequestMapping("/favourites")
 public class FavoriteController {
 
     private final FavoriteRepository favoriteRepository;
